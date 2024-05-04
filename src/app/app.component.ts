ddcbae69915e2580aea03824577f01e59085f48a
Janitha Madushanka <janitha.madushanka@circles.co>
2024-05-04 21:55:10 +0200

    changes  to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CellComponent } from './cell/cell.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CellComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
